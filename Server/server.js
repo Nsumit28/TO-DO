718a855876564d1d294bd9e717c554444064ccde
Nsumit28 <131881555+Nsumit28@users.noreply.github.com>
2024-11-06 19:41:08 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,38 @@
 
+// server/server.js
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const todoSchema = new mongoose.Schema({
+    task: String,
+    completed: Boolean,
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+app.get("/todos", async (req, res) => {
+    const todos = await Todo.find();
+    res.json(todos);
+});
+
+app.post("/todos", async (req, res) => {
+    const todo = new Todo({
+        task: req.body.task,
+        completed: false,
+    });
+    await todo.save();
+    res.json(todo);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
